1cf4a87db52e7d26f45e60071ca0cbae82ed2354
BlackzByt3 <blackbyte@blackzspace.de>
2024-04-19 22:56:49 +0200

    first commit

---
+++ p2pchat/p2pchat.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# p2pchat.py
+
+
+
+
+import os
+import sys
+import pathlib
+import subprocess
+import shlex
+import socket 
+import logging
+import hashlib
+import time
+
+import os.path
+
+from os.path import *
+from socket import *
+from subprocess import PIPE, Popen, STDOUT
+from time import sleep
+
+
+
+
+
+
+
+class Server:
+    def __init__(self):
+        print("test")
+
+
+
+class Client:
+    def __init__(self):
+        """"""
+
+
+
+
+
+
+class Main:
+    def __init__(self):
+        """"""
+
+    def start():
+        ix = Server()
+
+
+
+
+if __name__ == '__main__':
+    app = Main
+    app.start()
